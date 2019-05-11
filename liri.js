671a98e8980769785cd22fcd9165db8bcf4f7709
Andrew Kelly <drew12396@gmail.com>
2019-05-11 17:44:10 -0500

    spotify search added

--- liri.js
+++ liri.js
@@ -1,13 +1,40 @@
 require("dotenv").config();
+
 var Spotify = require('node-spotify-api');
-
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
+var type = process.argv.splice(2,1).join();
+var term = process.argv.slice(2).join(" ");
+
+
+
+  function spotifySearch(searchTerm) {
+
+    function setArtists(data) {
+        var artists = [];
+        for(var i = 0; i < data.tracks.items[0].artists.length; i++) {
+            artists.push(data.tracks.items[0].artists[i].name);
+        }
+        return artists;
     }
-   
-  console.log(data); 
-  });
+
+    var spotify = new Spotify(keys.spotify);
+
+    spotify.search({ type: 'track', query: searchTerm }, function(err, data) {
+        if (err) {
+        return console.log('Error occurred: ' + err);
+        }
+        var result = {
+            artist: [],
+            songName: data.tracks.items[0].name,
+            songLink: data.tracks.items[0].external_urls.spotify,
+            albumName: data.tracks.items[0].album.name
+        }
+        result.artist = setArtists(data);
+        console.log(result);
+    });
+  }
+
+
+
+  spotifySearch(term);
