6d0dfcce551c93cdd31badd726d7b162b68ab64d
Andrew Kelly <drew12396@gmail.com>
2019-05-11 18:00:25 -0500

    refactored spotify into class

--- liri.js
+++ liri.js
@@ -1,40 +1,11 @@
-require("dotenv").config();
-
-var Spotify = require('node-spotify-api');
-var keys = require("./keys.js");
+var SpotifySearch = require("./spotify");
 
 var type = process.argv.splice(2,1).join();
 var term = process.argv.slice(2).join(" ");
 
 
 
-  function spotifySearch(searchTerm) {
 
-    function setArtists(data) {
-        var artists = [];
-        for(var i = 0; i < data.tracks.items[0].artists.length; i++) {
-            artists.push(data.tracks.items[0].artists[i].name);
-        }
-        return artists;
-    }
+var songSearch = new SpotifySearch();
 
-    var spotify = new Spotify(keys.spotify);
-
-    spotify.search({ type: 'track', query: searchTerm }, function(err, data) {
-        if (err) {
-        return console.log('Error occurred: ' + err);
-        }
-        var result = {
-            artist: [],
-            songName: data.tracks.items[0].name,
-            songLink: data.tracks.items[0].external_urls.spotify,
-            albumName: data.tracks.items[0].album.name
-        }
-        result.artist = setArtists(data);
-        console.log(result);
-    });
-  }
-
-
-
-  spotifySearch(term);
+songSearch.spotifySearch(term);
---
+++ spotify.js
@@ -0,0 +1,68 @@
+require("dotenv").config();
+
+
+const Spotify = require('node-spotify-api');
+const keys = require("./keys.js");
+
+class SpotifySearch {
+    constructor() {
+        this.keys = keys
+    }
+    spotifySearch(searchTerm) {
+
+        function setArtists(data) {
+            var artists = [];
+            for(var i = 0; i < data.tracks.items[0].artists.length; i++) {
+                artists.push(data.tracks.items[0].artists[i].name);
+            }
+            return artists;
+        }
+    
+        var spotify = new Spotify(this.keys.spotify);
+    
+        spotify.search({ type: 'track', query: searchTerm }, function(err, data) {
+            if (err) {
+            return console.log('Error occurred: ' + err);
+            }
+            var result = {
+                artist: [],
+                songName: data.tracks.items[0].name,
+                songLink: data.tracks.items[0].external_urls.spotify,
+                albumName: data.tracks.items[0].album.name
+            }
+            result.artist = setArtists(data);
+            console.log(result);
+        });
+      }
+}
+
+
+
+// function spotifySearch(searchTerm) {
+
+//     function setArtists(data) {
+//         var artists = [];
+//         for(var i = 0; i < data.tracks.items[0].artists.length; i++) {
+//             artists.push(data.tracks.items[0].artists[i].name);
+//         }
+//         return artists;
+//     }
+
+//     var spotify = new Spotify(keys.spotify);
+
+//     spotify.search({ type: 'track', query: searchTerm }, function(err, data) {
+//         if (err) {
+//         return console.log('Error occurred: ' + err);
+//         }
+//         var result = {
+//             artist: [],
+//             songName: data.tracks.items[0].name,
+//             songLink: data.tracks.items[0].external_urls.spotify,
+//             albumName: data.tracks.items[0].album.name
+//         }
+//         result.artist = setArtists(data);
+//         console.log(result);
+//     });
+//   }
+
+  module.exports = SpotifySearch;
