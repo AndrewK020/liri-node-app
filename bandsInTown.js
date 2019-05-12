07de421d69539321f897209f90e246b0d9670a5c
Andrew Kelly <drew12396@gmail.com>
2019-05-11 19:02:09 -0500

    refactored event search into class
    added driver function to test

--- bandsInTown.js
+++ bandsInTown.js
@@ -1,17 +1,20 @@
 var axios = require("axios");
 var moment = require("moment");
 
-var artist = "khalid"
-axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-.then(
-  function(response) {
-    var event = response.data[0];
-    var result = {
-        venueName: event.venue.name,
-        location: event.venue.city + ", " + event.venue.region,
-        date: moment(event.datetime).format("MM-DD-YYYY")
+class BandInTown {
+    eventSearch(artist) {
+        axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+        .then(
+        function(response) {
+            var event = response.data[0];
+            var result = {
+                venueName: event.venue.name,
+                location: event.venue.city + ", " + event.venue.region,
+                date: moment(event.datetime).format("MM-DD-YYYY")
+            }
+            console.log(result);
+            }
+        );
     }
-
-    console.log(result);
-  }
-);
+}
+module.exports = BandInTown;
--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 var SpotifySearch = require("./spotify");
+var BandInTown = require("./bandsInTown");
 
 var type = process.argv.splice(2,1).join();
 var term = process.argv.slice(2).join(" ");
@@ -7,5 +8,6 @@ var term = process.argv.slice(2).join(" ");
 
 
 var songSearch = new SpotifySearch();
-
-songSearch.spotifySearch(term);
+var eventSearch = new BandInTown();
+//songSearch.spotifySearch(term);
+eventSearch.eventSearch(term);
