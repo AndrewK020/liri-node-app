1352a6f11bcb58dca1e9ca4c4d30c0988e7c9646
Andrew Kelly <drew12396@gmail.com>
2019-05-11 19:36:13 -0500

    combinded all api classes

--- javascript/spotify.js
+++ javascript/spotify.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 
-
 const Spotify = require('node-spotify-api');
 const keys = require("./keys.js");
 
--- liri.js
+++ liri.js
@@ -5,13 +5,20 @@ var Omdb = require("./javascript/omdb");
 var type = process.argv.splice(2,1).join();
 var term = process.argv.slice(2).join(" ");
 
-
-
-
-var songSearch = new SpotifySearch();
-var eventSearch = new BandInTown();
-var omdbSearch = new Omdb();
-
-//songSearch.spotifySearch(term);
-//eventSearch.eventSearch(term);
-omdbSearch.movieSearch(term);
+switch (type) {
+    case "concert-this":
+        var eventSearch = new BandInTown();
+        eventSearch.eventSearch(term);
+        break;
+    case "spotify-this-song":
+        var songSearch = new SpotifySearch();
+        songSearch.spotifySearch(term);
+        break;
+    case "movie-this":
+        var omdbSearch = new Omdb();
+        omdbSearch.movieSearch(term);
+        break;
+    default:
+        console.log("invalid search request\nplease try:\n'concert-this'\n'spotify-this-song'\n'movie-this'\nfollowed by your search term");
+        break;
+}
